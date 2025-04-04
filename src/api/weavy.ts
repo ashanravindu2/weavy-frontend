4d45d699fa0a9325508d465cde1323d1eac6b0bd
ashanravindu2 <ravindulakshithaashan@gmail.com>
2025-04-04 09:49:43 +0530

    services handle

--- src/api/weavy.ts
+++ src/api/weavy.ts
@@ -1,11 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
+import { User } from "../model/User";
 
-const weavyAPI = axios.create({
-    baseURL: 'https://8015b5dbc0724d38882ac90397c27649.weavy.io/api/users',
-    headers: {
-        Authorization: 'Bearer YOUR_ACCESS_TOKEN',
-        'Content-Type': 'application/json',
-    },
-});
+const API_URL = "https://8015b5dbc0724d38882ac90397c27649.weavy.io/api/users";
+const API_KEY = "wys_hMWpXdekxcn9Gc8Ioah3azOllzUZ7l3HN9yB";
 
-export default weavyAPI;
+const headers = {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "application/json",
+};
+
+const UserService = {
+    createUser: (userData: User) => axios.post<User>(API_URL, userData, { headers }),
+    getUsers: () => axios.get<User[]>(API_URL, { headers }),
+};
+
+export default UserService;
